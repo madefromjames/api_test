ca63dc7cba4660481c484b4b7497ab5c68e37632
madefromjames <madefromjames@gmail.com>
2024-10-25 13:06:19 +0100

    debug changes

--- ecommerce_API/customer/models.py
+++ ecommerce_API/customer/models.py
@@ -1,9 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
 class Customer(AbstractUser):
     phone_number = models.CharField(max_length=15)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name = 'customer_set',
+        blank = True,
+        help_text = 'The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name = 'customer_set',
+        blank = True,
+        help_text = 'Specific permissions for this user.',
+    )
 
     def __str__(self):
         return self.username
--- ecommerce_API/ecommerce_API/settings.py
+++ ecommerce_API/ecommerce_API/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     # all apps
     'product',
-    'user'
+    'user',
     'cart',
     'order',
 
