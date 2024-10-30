8e1dfac44fcd49e7f47ba39417e14fd9758cbf06
madefromjames <madefromjames@gmail.com>
2024-10-30 23:04:53 +0100

    allowed host update

--- ecommerce_API/settings.py
+++ ecommerce_API/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ax9ydd_%h2np+)&t=!elrmwjychj74(bu0hz6nbhez)4w3+y42
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-api-z1aw.onrender.com', 'localhost',]
 
 
 # Application definition
